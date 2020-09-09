c979d1bbffd2265596682f76de6cb6cd5e0831ad
iroskoshnyi <ivan.roskoshnyi@chromeriver.com>
2020-09-09 19:46:12 +0300

    added validation for the file"s structure

--- src/attendify_task_closure_noob/core.cljs
+++ src/attendify_task_closure_noob/core.cljs
@@ -2,33 +2,46 @@
   (:require [reagent.core :refer [atom]]
             [reagent.dom :refer [render]]
             [clojure.string :as str]
+            [goog.labs.format.csv :refer [parse]]
+            [clojure.spec.alpha :as s]
             [cljs.core.async :refer [put! chan <! >!]])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
-;; derived from https://mrmcc3.github.io/post/csv-with-clojurescript/
-;; and based on reagent-frontend template.
 
-;; dependencies from project.clj in addition to clojure, clojurescript, and reagent:
-;; [org.clojure/core.async "0.2.395"]
+(defn validate-label [v]
+  (-> v empty? not))
+(defn is-number [x]
+  (boolean (-> x js/parseInt js/Boolean)))
+
+(s/def ::content
+       (s/+
+        (s/spec
+         (s/cat :company validate-label :income is-number))))
+(s/def ::nested
+       (s/cat :head    (s/spec (s/tuple #(= "Company" %) #(= "Income" %)))
+              :content ::content))
+
+;(println (s/conform ::nested [["Company" "Income"] ["Acme" 2] ["Evil" 2]]))
 
 ;; atom to store file contents
-
-(def file-data (atom []))
-(def toggled-state (atom false))
+(defonce default-state {:isOpen false :file-name ""})
+(defonce file-data (atom []))
+(defonce app-state (atom default-state))
 
 ;; transducer to stick on a core-async channel to manipulate all the weird javascript
 ;; event objects --- basically just takes the array of file objects or something
 ;; that the incomprehensible browser API creates and grabs the first one, then resets things.
 (def first-file
-  (map (fn [e]
-         (let [target (.-currentTarget e)
-               file (-> target .-files (aget 0))]
-           (set! (.-value target) "")
-           file))))
+  (map
+   (fn [e]
+     (let [target (.-currentTarget e)
+           file   (-> target .-files (aget 0))]
+       (set! (.-value target) "")
+       file))))
 
 ;; transducer to get text out of file object.
 (def extract-result
-  (map #(-> % .-target .-result js->clj)))
+  (map #(-> % .-target .-result parse js->clj)))
 
 ;; two core.async channels to take file array and then file and apply above transducers to them.
 (def upload-reqs (chan 1 first-file))
@@ -41,132 +54,116 @@
 ;; sit around in a loop waiting for a file to appear in the upload-reqs channel, read any such file, and when the read is successful, stick the file on the file-reads channel.
 (go-loop []
          (let [reader (js/FileReader.)
-               file (<! upload-reqs)]
+               file   (<! upload-reqs)]
+           (swap! app-state assoc :file-name (.-name file))
            (set! (.-onload reader) #(put! file-reads %))
            (.readAsText reader file)
            (recur)))
 
-;; input component to allow users to upload file.
-(defn input-component []
-  [:input {:type "file" :id "file" :accept ".txt" :name "file" :on-change put-upload}])
+;; sit around in a loop waiting for a string to appear in the file-reads channel and put it in the state atom to be read by reagent and rendered on the page.
+(go-loop []
+         (let [file (<! file-reads)]
+           (try
+             (if (not (s/valid? ::nested file))
+               (throw (js/Error. "The file is invalid")))
+             (let [parsedFile (vec
+                               (map
+                                (fn [item]
+                                  {:company (item 0) :income (item 1)})
+                                file))]
+               (reset! file-data parsedFile))
+             (catch js/Error e
+               (reset! file-data [])
+               (-> e .-message js/alert)))
+           (recur)))
 
 (defn on-item-click [e]
   (let [index (js/parseInt (-> e .-currentTarget .-dataset .-index))]
-    (reset! toggled-state (if (= @toggled-state index)
-                            false
-                            index
-                            ))
-    )
-  )
+    (swap! app-state assoc :isOpen
+           (if (= (get @app-state :isOpen) index)
+             false
+             index))))
 
 ;; ----Add a multi-method
 (defn table-item [el text index column]
   [el
-   (if (true? (= @toggled-state index))
+   (if (true? (= (get @app-state :isOpen) index))
      [:<>
-      [:input {
-                :value text
-                :type (if (and (= column :income) (= el :td))
-                        "number"
-                        "text"
-                        )
-                :on-blur (fn [e]
-                           (let [inputType (-> e .-currentTarget .-type)
-                                 v (-> e .-currentTarget .-value)
-                                 ]
-                             (when (and (= inputType "number") (empty? v))
-                                   (reset! file-data (
-                                     update-in @file-data [index column] (fn [oldVal] 0)
-                                   )
-                                 )
-                               )
-                             )
-                           )
-                :on-change (fn [e]
-                             (let [v (-> e .-currentTarget .-value)
-                                   inputType (-> e .-currentTarget .-type)
-                                   ]
-                               (reset! file-data (
-                                   update-in @file-data [index column] (fn [oldVal] v)
-                                   )
-                                 )
-                               )
-                             )
-                }
-       ]
-       [:span.toggle {
-                :on-click on-item-click
-                :data-index index
-                } "close"
-        ]
-      ]
+      [:input
+       {:value     text
+        :type      (if (and (= column :income) (= el :td))
+                     "number"
+                     "text")
+        :on-blur   (fn [e]
+                     (let [inputType (-> e .-currentTarget .-type)
+                           v         (-> e .-currentTarget .-value)]
+                       (reset! file-data
+                               (update-in @file-data [index column]
+                                          (fn [oldVal]
+                                            (if (and (= inputType "number") (empty? v))
+                                              0
+                                              (js/parseInt v)))))))
+        :on-change (fn [e]
+                     (let [v         (-> e .-currentTarget .-value)
+                           inputType (-> e .-currentTarget .-type)]
+                       (reset! file-data
+                               (update-in @file-data [index column] (fn [oldVal] v)))))}]
+      [:i.fas.fa-splotch
+       {:on-click   on-item-click
+        :data-index index}]]
      [:<>
       [:span text]
-      [:span.toggle {
-               :on-click on-item-click
-               :data-index index
-               } "open"
-       ]
-      ]
-     )
-   ]
-  )
+      [:i.fas.fa-pen
+       {:on-click   on-item-click
+        :data-index index}]])])
+
 ;; -------------------------
 ;; Views
-(defn table []
-  (when (first @file-data)
-    [:table.full-width.talign-center
-     [:thead
-      (let [{c :company i :income} (first @file-data)]
-        [:tr
-         [table-item :th c 0 :company]
-         [table-item :th i 0 :income]
-         ]
-        )
-      ]
-     [:tbody
-      (map-indexed (
-         fn [index {c :company i :income}]
-           ^{:key index} [:tr
-                          [table-item :td c (+ index 1) :company]
-                          [table-item :td i (+ index 1) :income]
-                          ]
-           ) (rest @file-data)
-         )
-      ]
-     ]
-    )
-  )
-
-;; sit around in a loop waiting for a string to appear in the file-reads channel and put it in the state atom to be read by reagent and rendered on the page.
-(go-loop []
-   (let [splitedArr (str/split (str/trim (<! file-reads)) "\n")]
-     (reset! file-data
-             (vec (map (fn [item]
-                 (let [[company income] (str/split item " ")]
-                   {:company company :income income}
-                   )
-                 )
-                splitedArr
-               ))
-       )
-     (recur)
- ))
+(defn table [header content]
+  (when header
+        [:table.full-width.talign-center
+         [:thead
+          (let [{c :company i :income} header]
+            [:tr
+             [table-item :th c 0 :company]
+             [table-item :th i 0 :income]])]
+         [:tbody
+          (map-indexed
+           (fn [index {c :company i :income}]
+             ^{:key index} [:tr
+                            [table-item :td c (+ index 1) :company]
+                            [table-item :td i (+ index 1) :income]])
+           content)]]))
+(defn upload-btn [file-name]
+  [:span.upload-label
+   [:label
+    [:input.d-none
+     {:type "file" :accept ".txt" :on-change put-upload}]
+    [:i.fa.fa-upload.fa-lg]
+    (if (empty? file-name)
+      "click here to upload and render csv..."
+      file-name)]
+   (when file-name
+         [:i.fa.fa-times.ml-2
+          {:on-click (fn [e]
+                       (reset! file-data [])
+                       (reset! app-state default-state))}])])
 
 (defn home-page []
-  [:div
-   [input-component]
-   [:p "total " (->> (rest @file-data)
-                    (map (fn [{i :income}]
-                           (if (= i "")
-                             0
-                             (js/parseInt i)
-                             )
-                           ))
-                    (reduce + 0)
-                    )]
-   [table]
-   ])
+  (let [{file-name :file-name} @app-state
+        [header & content]     @file-data]
+    [:div
+     [upload-btn file-name]
+     [:p
+      "total "
+      (->> content
+           (map
+            (fn [{i :income}]
+              (if (= i "")
+                0
+                (js/parseInt i))))
+           (reduce + 0))]
+     [table header content]]))
 
 ;; -------------------------
 ;; Initialize app
